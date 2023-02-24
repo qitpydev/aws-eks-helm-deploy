fd4a7ee1388136a73b2101b5454eb01f86c3abb3
quyet <code.maito@gmail.com>
2023-02-24 17:17:58 +0700

    release v1

--- bin/install_chart.sh
+++ bin/install_chart.sh
@@ -1,46 +1,28 @@
 #!/bin/bash
-# Parse command line arguments
-OPTIONS=$(getopt -o r:c:n:v: --long repository:,chart-name:,namespace:,chart-version: -- "$@")
-eval set -- "$OPTIONS"
-while true; do
-  case "$1" in
-    -r|--repository)
-      REPOSITORY="$2"; shift 2;;
-    -c|--chart-name)
-      CHART_NAME="$2"; shift 2;;
-    -n|--namespace)
-      NAMESPACE="$2"; shift 2;;
-    -v|--chart-version)
-      CHART_VERSION="$2"; shift 2;;
-    --)
-      shift; break;;
-    *)
-      echo "Invalid argument: $1" >&2; exit 1;;
-  esac
-done
-
-# Check if required arguments are present
-if [[ -z "$REPOSITORY" ]]; then
-  echo "Repository argument is missing." >&2
-  exit 1
-fi
-if [[ -z "$CHART_NAME" ]]; then
-  echo "Chart name argument is missing." >&2
-  exit 1
-fi
-if [[ -z "$NAMESPACE" ]]; then
-  echo "Namespace argument is missing." >&2
-  exit 1
-fi
-if [[ -z "$CHART_VERSION" ]]; then
-  echo "Chart version argument is missing." >&2
-  exit 1
-fi
+# # Parse command line arguments
+# OPTIONS=$(getopt -o r:c:n:v: --long repository:,chart-name:,namespace:,chart-version: -- "$@")
+# eval set -- "$OPTIONS"
+# while true; do
+#   case "$1" in
+#     -r|--repository)
+#       REPOSITORY="$2"; shift 2;;
+#     -c|--chart-name)
+#       CHART_NAME="$2"; shift 2;;
+#     -n|--namespace)
+#       NAMESPACE="$2"; shift 2;;
+#     -v|--chart-version)
+#       CHART_VERSION="$2"; shift 2;;
+#     --)
+#       shift; break;;
+#     *)
+#       echo "Invalid argument: $1" >&2; exit 1;;
+#   esac
+# done
 
 # Create the Helm chart
-helm create $CHART_NAME
+helm create $EKS_CHART_NAME
 
-helm upgrade --install $CHART_NAME $CHART_NAME \
-    --set image.repository=$REPOSITORY \
-    --set image.tag=$CHART_VERSION \
-    --namespace $NAMESPACE
+helm upgrade --install $EKS_CHART_NAME $EKS_CHART_NAME \
+    --set image.repository=$EKS_IMAGE_REPOSITORY \
+    --set image.tag=$EKS_CHART_VERSION \
+    --namespace $EKS_NAMESPACE
---
+++ test.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+if [[ -z "${AWS_ACCESS_KEY_ID}" ]]; then
+  echo "Error: AWS_ACCESS_KEY_ID environment variable is not set"
+  exit 1
+fi
+
+if [[ -z "${AWS_SECRET_ACCESS_KEY}" ]]; then
+  echo "Error: AWS_SECRET_ACCESS_KEY environment variable is not set"
+  exit 1
+fi
+
+if [[ -z "${AWS_DEFAULT_REGION}" ]]; then
+  echo "Error: AWS_DEFAULT_REGION environment variable is not set"
+  exit 1
+fi
+
+if [[ -z "${KUBECONFIG_FILE_S3_URL}" ]]; then
+  echo "Error: KUBECONFIG_FILE_S3_URL environment variable is not set"
+  exit 1
+fi
+
+# Check if required arguments are present
+if [[ -z "$EKS_IMAGE_REPOSITORY" ]]; then
+  echo "Repository argument is missing."
+  exit 1
+fi
+if [[ -z "$EKS_CHART_NAME" ]]; then
+  echo "Chart name argument is missing."
+  exit 1
+fi
+if [[ -z "$EKS_NAMESPACE" ]]; then
+  echo "Namespace argument is missing."
+  exit 1
+fi
+if [[ -z "$EKS_CHART_VERSION" ]]; then
+  echo "Chart version argument is missing."
+  exit 1
+fi
