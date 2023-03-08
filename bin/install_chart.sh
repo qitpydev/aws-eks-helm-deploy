cf15ff76774da94320f263e0d174b37d83f24481
quyet <code.maito@gmail.com>
2023-03-08 14:37:06 +0700

    feat: make container_port is optional

--- bin/install_chart.sh
+++ bin/install_chart.sh
@@ -20,6 +20,8 @@
 # done
 
 # Create the Helm chart
+if [ -z ${EKS_CONTAINER_PORT+x} ]; then EKS_CONTAINER_PORT=80; fi
+
 helm create $EKS_CHART_NAME
 
 helm upgrade --install $EKS_CHART_NAME $EKS_CHART_NAME \
