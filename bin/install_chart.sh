61ae3a1c950bd001eddb4f674ac5c2d81f027cc3
quyet <code.maito@gmail.com>
2023-03-09 11:47:16 +0700

    hotfix: debug

--- bin/install_chart.sh
+++ bin/install_chart.sh
@@ -9,6 +9,7 @@ echo 'PrePare'
 if [ -z ${EKS_CONTAINER_PORT+x} ]; then EKS_CONTAINER_PORT=80; fi
 sed -i "s/containerPort: 80/containerPort: $EKS_CONTAINER_PORT/g" ./$EKS_CHART_NAME/templates/deployment.yaml
 sed -i "s/containerPort: 80/containerPort: $EKS_CONTAINER_PORT/g" ./$EKS_CHART_NAME/templates/deployment.yaml
+cat ./$EKS_CHART_NAME/templates/deployment.yaml
 echo 'PASSSSSSSS'
 
 # Installing
