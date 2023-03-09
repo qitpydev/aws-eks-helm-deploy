83ff0bdf2b1a4c99f0abd4b5a53eb71ac1a34183
quyet <code.maito@gmail.com>
2023-03-09 12:59:49 +0700

    hotfix: debug

--- bin/install_chart.sh
+++ bin/install_chart.sh
@@ -4,9 +4,14 @@
 if [ -z ${EKS_CHART_VERSION+x} ]; then EKS_CHART_VERSION=latest; fi
 helm create $EKS_CHART_NAME
 
-# Setting container_port (if having)
+# Setting container_port (optional)
 if [ -z ${EKS_CONTAINER_PORT+x} ]; then EKS_CONTAINER_PORT=80; fi
 sed -i "s/containerPort: 80/containerPort: $EKS_CONTAINER_PORT/g" ./$EKS_CHART_NAME/templates/deployment.yaml
+
+# Setting livenessProbe vs readinessProbe (optional)
+if [ -n "$EKS_CONTAINER_HEALTH_CHECK_PATH" ]; then
+    sed -i 's|path: /|path: $APP_HEALTH_CHECK_PATH|g' ./$EKS_CHART_NAME/templates/deployment.yaml
+fi
 
 # Installing
 helm upgrade --install $EKS_CHART_NAME $EKS_CHART_NAME \
--- bin/setting_container_port.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-if [ -z ${EKS_CONTAINER_PORT+x} ]; then EKS_CONTAINER_PORT=80; fi
-# Replace the containerPort value with the new value
-sed -i "s/containerPort: 80/containerPort: $EKS_CONTAINER_PORT/g" ./$EKS_CHART_NAME/templates/deployment.yaml
