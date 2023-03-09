367339eade26063242cee62be33e82871edc4d97
quyet <code.maito@gmail.com>
2023-03-09 11:42:26 +0700

    hotfix: debug

--- bin/install_chart.sh
+++ bin/install_chart.sh
@@ -7,6 +7,7 @@ helm create $EKS_CHART_NAME
 # Setting container_port (optionals)
 if [ -z ${EKS_CONTAINER_PORT+x} ]; then EKS_CONTAINER_PORT=80; fi
 sed -i "s/containerPort: 80/containerPort: $EKS_CONTAINER_PORT/g" ./$EKS_CHART_NAME/templates/deployment.yaml
+sed -i "s/containerPort: 80/containerPort: $EKS_CONTAINER_PORT/g" ./$EKS_CHART_NAME/templates/deployment.yaml
 
 # Installing
 helm upgrade --install $EKS_CHART_NAME $EKS_CHART_NAME \
