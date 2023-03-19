150768d5709fd26d249a356104a0e6d0152da462
quyet <code.maito@gmail.com>
2023-03-19 23:08:35 +0700

    config to run check pod running

--- test/check_result.sh
+++ test/check_result.sh
@@ -1,17 +1,19 @@
-POD_STATUS=""
+#!/bin/bash
 
 while true; do
-  POD_STATUS=$(kubectl get pod -l app=$POD_NAME -o jsonpath='{.items[0].status.phase}')
-  if [[ $POD_STATUS == "Pending" || $POD_STATUS == "Running" ]]; then
-    break
-  fi
-  echo "Waiting for pod to be running. Current status: $POD_STATUS"
-  sleep 5
+    POD_STATUS=$(kubectl get pod -o jsonpath='{.items[0].status.phase}')
+    case "$POD_STATUS" in
+        Pending)
+            echo "Waiting for pod to be running. Current status: $POD_STATUS"
+            sleep 5
+            ;;
+        Running)
+            echo "Pod is now running. Status: $POD_STATUS"
+            exit 0
+            ;;
+        *)
+            echo "Error: Pod status is $POD_STATUS"
+            exit 1
+            ;;
+    esac
 done
-
-if [[ $POD_STATUS != "Pending" && $POD_STATUS != "Running" ]]; then
-  echo "Error: Pod status is $POD_STATUS"
-  exit 1
-fi
-
-echo "Pod is now running. Status: $POD_STATUS"
