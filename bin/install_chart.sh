1ba0c21faa136f9076efdbf8a643c8e404271ba8
quyet <code.maito@gmail.com>
2023-03-28 12:46:28 +0700

    fix case var is empty string

--- bin/install_chart.sh
+++ bin/install_chart.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Create the Helm chart
-if [ -z ${EKS_CHART_VERSION+x} ]; then EKS_CHART_VERSION=latest; fi
+if [ -z ${EKS_CHART_VERSION} ]; then EKS_CHART_VERSION=latest; fi
 helm create $EKS_CHART_NAME
 
 # Setting container_port (optional)
-if [ -z ${EKS_CONTAINER_PORT+x} ]; then EKS_CONTAINER_PORT=80; fi
+if [ -z ${EKS_CONTAINER_PORT} ]; then EKS_CONTAINER_PORT=80; fi
 sed -i "s/containerPort: 80/containerPort: $EKS_CONTAINER_PORT/g" ./$EKS_CHART_NAME/templates/deployment.yaml
 
 # Setting livenessProbe vs readinessProbe (optional)
-if [ -n "${EKS_CONTAINER_HEALTH_CHECK_PATH+x}" ]; then
+if [ -n "${EKS_CONTAINER_HEALTH_CHECK_PATH}" ]; then
     sed -i "s|path: /|path: $EKS_CONTAINER_HEALTH_CHECK_PATH|g" ./$EKS_CHART_NAME/templates/deployment.yaml
 fi
 
@@ -19,19 +19,19 @@ helm_args="$helm_args --set image.tag=$EKS_CHART_VERSION"
 helm_args="$helm_args --set service.port=$EKS_CONTAINER_PORT"
 helm_args="$helm_args --namespace $EKS_NAMESPACE"
 
-if [ -n "${RESOURCE_LIMITS_CPU+x}" ]; then
+if [ -n "${RESOURCE_LIMITS_CPU}" ]; then
     helm_args="$helm_args --set resources.limits.cpu=${RESOURCE_LIMITS_CPU}"
 fi
 
-if [ -n "${RESOURCE_LIMITS_MEMORY+x}" ]; then
+if [ -n "${RESOURCE_LIMITS_MEMORY}" ]; then
     helm_args="$helm_args --set resources.limits.memory=${RESOURCE_LIMITS_MEMORY}"
 fi
 
-if [ -n "${RESOURCE_REQUEST_CPU+x}" ]; then
+if [ -n "${RESOURCE_REQUEST_CPU}" ]; then
     helm_args="$helm_args --set resources.requests.cpu=${RESOURCE_REQUEST_CPU}"
 fi
 
-if [ -n "${RESOURCE_REQUEST_MEMORY+x}" ]; then
+if [ -n "${RESOURCE_REQUEST_MEMORY}" ]; then
     helm_args="$helm_args --set resources.requests.memory=${RESOURCE_REQUEST_MEMORY}"
 fi
 
