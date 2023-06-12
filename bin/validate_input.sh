8750f5094949763a6d0185d3ef733120827771a2
quyet <code.maito@gmail.com>
2023-06-12 11:51:57 +0700

    :construction_worker: Checking shell script

--- bin/validate_input.sh
+++ bin/validate_input.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 
 if [[ -z "${AWS_ACCESS_KEY_ID}" ]]; then
-  echo "Error: AWS_ACCESS_KEY_ID environment variable is not set"
+  echo "Error: AWS_ACCESS_KEY_ID argument is missing"
   exit 1
 fi
 
 if [[ -z "${AWS_SECRET_ACCESS_KEY}" ]]; then
-  echo "Error: AWS_SECRET_ACCESS_KEY environment variable is not set"
+  echo "Error: AWS_SECRET_ACCESS_KEY argument is missing"
   exit 1
 fi
 
 if [[ -z "${AWS_DEFAULT_REGION}" ]]; then
-  echo "Error: AWS_DEFAULT_REGION environment variable is not set"
+  echo "Error: AWS_DEFAULT_REGION argument is missing"
   exit 1
 fi
 
 if [[ -z "${KUBECONFIG_FILE_S3_URL}" ]]; then
-  echo "Error: KUBECONFIG_FILE_S3_URL environment variable is not set"
+  echo "Error: KUBECONFIG_FILE_S3_URL argument is missing"
   exit 1
 fi
 
-# Check if required arguments are present
 if [[ -z "$EKS_IMAGE_REPOSITORY" ]]; then
   echo "Repository argument is missing."
   exit 1
